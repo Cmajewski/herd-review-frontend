ee4411b33657b366c4a759b8b9445029e7ee96fb
github username <github email address>
2020-07-30 21:06:50 -0400

    postFetch and formHandler

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 let btnStatus = false;
 
 document.addEventListener("DOMContentLoaded",()=>{
-  getProducts()
+  getProducts();
+
+  const form=document.querySelector(".add-product-form");
   const addProductBtn = document.querySelector("#new-product-btn");
+  const container = document.querySelector(".container");
         
-        const container = document.querySelector(".container");
-        
-        addProductBtn.addEventListener("click", () => {
-          btnStatus = !btnStatus;
-          if (btnStatus) {
-            container.style.display = "block";
-          } else {
-            container.style.display = "none";
-          }
-        });
-  
+  form.addEventListener("submit",e=>{
+  formHandler(e)
+  })
+
+  addProductBtn.addEventListener("click", () => {
+
+    btnStatus = !btnStatus;
+    if (btnStatus) {
+      container.style.display = "block";
+    } else {
+      container.style.display = "none";
+    }
+  });
+       
 })
 
-
+ 
 function getProducts(){
     fetch ("http://localhost:3000/products")
     .then(response=>response.json())
@@ -28,6 +34,35 @@ function getProducts(){
 
   function createproductCard(){
    
+  }
+
+  function formHandler(e){
+    event.preventDefault()
+    const name=document.querySelector(".add-product-form").name.value
+    const brand=document.querySelector(".add-product-form").brand.value
+    const category=document.querySelector(".add-product-form").category.value
+    const description=document.querySelector(".add-product-form").description.value
+    const image=document.querySelector(".add-product-form").image.value
+    postFetch(name,brand,image,description)
+  }
+
+  function postFetch(name,brand,category,description,image_url){
+    fetch("http://localhost:3000/products",{
+      method: "Post",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        brand:brand,
+        category: category,
+        description: description,
+        image_url: image_url,
+        likes: 0
+      })
+    });
+     
   }
 
 
